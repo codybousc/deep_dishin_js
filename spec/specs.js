a240adf8f260f2c6951088b2e44663cd0bc1b67a
Cody Bouscaren <codybousc@gmail.com>
2015-06-05 10:19:38 -0700

    adds background photo

--- spec/specs.js
+++ spec/specs.js
@@ -3,6 +3,12 @@ describe("PizzaPie", function() {
     var testPizza = new PizzaPie(10);
     expect(testPizza.basePrice).to.equal(10);
   });
+  
+  it("returns the price of Large pizza", function(){
+    var testPizza = new PizzaPie(10);
+    expect(testPizza.Small()).to.equal(15);
+    debugger;
+  });
 
 
 });
