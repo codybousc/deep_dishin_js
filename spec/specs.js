5afa93c81f3318b9af54e1c19d57f8e940166da1
Cody Bouscaren <codybousc@gmail.com>
2015-06-05 10:46:26 -0700

    passes toppings spec

--- spec/specs.js
+++ spec/specs.js
@@ -7,13 +7,16 @@ describe("PizzaPie", function() {
   it("returns the size of a Large pizza", function(){
     var testPizza = new PizzaPie("Large");
     expect(testPizza.pizza).to.equal("Large");
-
   });
 
   it("returns the size of a Small pizza", function(){
     var testPizza = new PizzaPie("Small");
     expect(testPizza.pizza).to.equal("Small");
+  });
 
+  it("returns the pizza with chosen toppings", function(){
+    var testPizza = new PizzaPie("Meat Lover");
+    expect(testPizza.pizza).to.equal("Meat Lover");
   });
 
 
