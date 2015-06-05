c614d2f079a4f677bdde503088b5ac80ce3165bd
Cody Bouscaren <codybousc@gmail.com>
2015-06-05 09:56:10 -0700

    Initial file structure set

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+function PizzaPie(pizza) {
+  this.basePrice = 10;
+  this.pizza = pizza;
+}
+
+PizzaPie.prototype.Large = function() {
+  this.basePrice = this.basePrice + 5;
+}
+
+PizzaPie.prototype.Small = function() {
+  this.basePrice = this.basePrice - 2;
+}
+
+$(document).ready(function(){
+  $("form#new_pizza").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedPizza_size = $("#pizza_size").val();
+    var inputtedToppings = $("#toppings").val();
+
+    var newPizza = new PizzaPie(inputtedPizza_size, inputtedToppings);
+
+    if (inputtedPizza_size === "Large") {
+       newPizza.Large();
+     }
+     else if (inputtedPizza_size === "Small") {
+       newPizza.Small();
+    } else {
+      newPizza.basePrice;
+    }
+
+
+
+
+    $("ul#order").append("<li><span class='pizza'>" + newPizza.pizza + " " + inputtedToppings + " " + "$" + newPizza.basePrice + ".00"  )
+
+    $("input#pizza_size").val("");
+    $("input#toppings").val("");
+  });
+});
